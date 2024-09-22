08d847d26f6156c1439555c56f540e7b8de87068
N Mouli <66863784+mouli-n@users.noreply.github.com>
2024-09-23 00:27:28 +0530

    Update ColumnTable.jsx

--- Table/ColumnTable.jsx
+++ Table/ColumnTable.jsx
@@ -1,1 +1,15 @@
+function ColumnData({ columns }) {
+    return (
+        <div style={{ display: "flex", justifyContent: "space-evenly", background: "#000" }}>
+            {
+                columns.map((e) =>
+                    <div style={{ background: "#eee", margin: "5px", width: "50%" }}>
+                        {e.name}
+                    </div>)
+            }
+        </div>
+    );
+}
+
+export default ColumnData;
 
